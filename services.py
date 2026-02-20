b19c96ff14e12f11721939e23d35c24b0e7734e2
ap25016 <ap25016@hua.gr>
2026-02-20 16:52:58 +0200

    Update services.py

--- services.py
+++ services.py
@@ -1,6 +1,1 @@
-def external_validator_lib(barcode: str):
-    """Προσομοίωση εξωτερικής βιβλιοθήκης ελέγχου barcode"""
-    return len(barcode) > 0
-def notification_service_lib(notif_obj, message: str):
-    """Προσομοίωση εξωτερικής βιβλιοθήκης ειδοποιήσεων"""
-    print(f"\n>>> [NOTIF-ID: {notif_obj.notif_id}] {message}")
+from models import Shelf, Palette, ProductCategory, Notification, Driver
