48ec177d84bed453c08317ef0d5642e919e5639c
ap25001hua <user.email>
2026-02-22 01:35:09 +0000

    Ολοκλήρωση Use Case 2 και README

--- main.py
+++ main.py
@@ -75,19 +75,6 @@ def main():
                             print("\nΕπιτυχής ολοκλήρωση παραλαβής!")
                             print(f"Η παραλαβη ολοκληρώθηκε στις {now}.")
                             break
-
-                 elif choice == "2":
-                    print("\nΛυπούμαστε, η συγκεκριμένη λειτουργία του συστήματος δεν έχει ολοκληρωθεί ακόμα!")
-
-                 elif choice == "0":
-                    print("\nΑντίο!")
-                    break
-                 else:
-                    print("\nΜη έγκυρη επιλογή.")
-
-
-
-        #~-~-~-~-~-~-~-~-~-~-~-~-~-~-ΤΟ USE CASE 2 ΞΕΚΙΝΑΕΙ ΑΠΟ ΕΔΩ-~-~-~-~-~-~-~-~-~-~-~-~-~-~#
         elif choice == "2":
             print("\nΠαρακαλώ εισάγετε τα στοιχεια σας:")
             try:
@@ -105,6 +92,50 @@ def main():
             else:
                 print("\nΕπιτυχής Σύνδεση!")
 
+                while True:
+                        storage_notifs = len(system.notifications) #η len μας δινει τον αριθμο των ειδοποιησεων
+                        print(f"\nΕιδοποιήσεις Αποθήκευσης: {storage_notifs}") #ποσες ειδοποιησεις υπαρχουν
+                        print("Ειδοποιήσεις Παραγγελιών: 0") #αλλο use case
+                        print("Επιλέξτε Ενέργεια:")
+                        print("1. Αποθήκευση Προϊόντων")
+                        print("2. Εκτέλεση Παραγγελίας")
+                        print("0. Έξοδος")
+                        choice = input("Επιλογή: ")
+                        
+                        if choice == "1":
+                            if storage_notifs == 0:
+                                print("\nΔεν υπάρχουν παλέτες σε εκκρεμότητα!")
+                                continue
+                            new_notif = system.notifications[0] #αυτο μας δινει τη πρωτη ειδοποιηση που υπαρχει στο συστημα
+                            
+                            while new_notif.palettes: #αυτο κανει loop τη διαδικασια για όσο υπάρχουν παλέτες μέσα στην ειδοποίηση
+                                print(f"\n{new_notif.notification_time.strftime('%H:%M:%S, %d-%m-%Y')}")
+                                print("Προϊόντα προς Αποθήκευση:")
+                                
+                                for idx, p in enumerate(new_notif.palettes, 1): #αυτο δειχνει τη λιστα με τις παλετες απο την ειδοποιηση
+                                    print(f"{idx}. Παλέτα: {p.barcode}, Κατηγορία: {p.category.category_name} "
+                                          f"(ID: {p.category.category_id}), Κωδικός Ραφιού: {p.shelf.shelf_id}")
+                                          
+                                p_choice = input("Παρακαλώ επιλέξτε παλέτα για αποθήκευση: ")
+                                try:
+                                    p_idx = int(p_choice) - 1
+                                    selected_palette = new_notif.palettes[p_idx]
+                                    success = driver.confirm_placement(selected_palette, new_notif) #αυτο εκτελει την αποθηκευση των δεδομενων των παλετων και την ενημερωση του αποθεματος
+                                    if success:
+                                        new_notif.palettes.pop(p_idx) #αυτο αφαιρει τη παλετα απο τη λιστα
+                                except (ValueError, IndexError):
+                                    print("Μη έγκυρη επιλογή.")
+                            print("\nΔεν υπάρχουν παλέτες σε εκκρεμότητα!") 
+                            system.notifications.pop(0) #αυτο αφαιρει την ειδοποιηση απο το συστημα οταν δεν υπαρχουν αλλες παλετες σε αυτη
+                            print("Γίνεται επιστροφή στο αρχικό μενού...")
+                            break
+                        
+                        elif choice == "2":
+                            print("\nΔεν υπάρχουν εκκρεμείς ααραγγελίες!")
+                        elif choice == "0":
+                            print("\nΑντίο!")
+                            break
+
         
         elif choice == "0":
             print("\nΠραγματοποιειται έξοδος από το σύστημα. Αντίο!")
@@ -112,7 +143,5 @@ def main():
 
         else:
             print("\nΜη έγκυρη επιλογή.")
-
-
 if __name__ == "__main__":
     main()
